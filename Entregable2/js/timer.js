bd652d6f47aaf2cb61a5978acd88854201b5f4ce
Sofia Martinez <sofiainesmartinez@gmail.com>
2022-09-29 16:58:40 -0300

    Se agrega comportamiento a timer

--- Entregable2/js/timer.js
+++ Entregable2/js/timer.js
@@ -1,14 +1,20 @@
 "use strict";
 
-let timer = document.getElementById("timer");
-let numbers = ['04', '03', '02', '01', '00'];
-
 function countdown() {
-    let index = 0;
-    while (index < 5) {
-        timer.innerText = numbers[index];
-        setTimeout(index++, 1000);
-    }
-};
+    let timer = document.getElementById("timer");
+    let i = 27;
+    setInterval(function () {
+        timer.innerText = i;
+        if (i == 0) {
+            i = 59;
+        }
+        else if (i >= 10) {
+            i--;
+        } else {
+            timer.innerText = `0${i}`;
+            i--;
+        }
+    }, 1000);
+}
 
 countdown();
