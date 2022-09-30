b5eca2156ed86d4bd9ae69c569a7444ea0abc411
Tomás Rodríguez <augustotomasrodriguez@gmail.com>
2022-09-30 20:18:25 -0300

    Fixed bug heart animation.

--- Entregable2/js/heartAnimation.js
+++ Entregable2/js/heartAnimation.js
@@ -1,14 +1,11 @@
 "use strict";
 
-
-const heart = document.getElementById('vectorHeart');
-let toggle = true;
-
-heart.addEventListener('click', function() {
-    toggle = !toggle;
-    if (toggle) {
-        heart.src = 'img/icons/heart-regular.svg';
-    } else {
-        heart.src = 'img/icons/heart-solid.svg';
-    }
+document.querySelectorAll('.vectorHeart').forEach(item => {
+    item.addEventListener('click', event => {
+        if (item.getAttribute("src") == 'img/icons/heart-regular.svg') {
+            item.src = 'img/icons/heart-solid.svg';
+        } else {
+            item.src = 'img/icons/heart-regular.svg';
+        }
+    })
 })
