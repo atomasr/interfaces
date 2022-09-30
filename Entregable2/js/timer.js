4371c45447e01681f6b570d552113c8ba1d1dbba
Tomás Rodríguez <augustotomasrodriguez@gmail.com>
2022-09-30 19:07:18 -0300

    00 arreglado timer.

--- Entregable2/js/timer.js
+++ Entregable2/js/timer.js
@@ -3,12 +3,12 @@
 function countdown() {
     let timer = document.getElementById("timer");
     let i = 27;
-    setInterval(function () {
+    setInterval(function() {
         timer.innerText = i;
         if (i == 0) {
+            timer.innerText = `00`;
             i = 59;
-        }
-        else if (i >= 10) {
+        } else if (i >= 10) {
             i--;
         } else {
             timer.innerText = `0${i}`;
