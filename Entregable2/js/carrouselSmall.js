bed84df9d89dfb1a43907deaca5946962248302d
Tomás Rodríguez <augustotomasrodriguez@gmail.com>
2022-10-01 00:55:17 -0300

    Complete los juegos y su info.

--- Entregable2/js/carrouselSmall.js
+++ Entregable2/js/carrouselSmall.js
@@ -52,199 +52,199 @@ let carrousel1 = [{
     },
 ]
 let carrousel2 = [{
-        "picture": "01A",
-        "hover": "01B",
-        "name": "Among Us",
-        "category": "Multiplayer",
+        "picture": "07A",
+        "hover": "07B",
+        "name": "Tetris",
+        "category": "Puzzle",
         "price": "Free",
         "cents": ""
     },
     {
-        "picture": "02A",
-        "hover": "02B",
-        "name": "Fornite",
-        "category": "Multiplayer",
-        "price": "$1.",
-        "cents": "99"
-    },
-    {
-        "picture": "03a",
-        "hover": "03B",
-        "name": "Minecraft",
-        "category": "Multiplayer",
+        "picture": "08A",
+        "hover": "08B",
+        "name": "Breaking Bad: 4 in Line",
+        "category": "Classic",
         "price": "Free",
         "cents": ""
     },
     {
-        "picture": "04A",
-        "hover": "04B",
-        "name": "Fall Guys",
-        "category": "Multiplayer",
+        "picture": "09A",
+        "hover": "09B",
+        "name": "Cut The Rope",
+        "category": "Puzzle",
+        "price": "Free",
+        "cents": ""
+    },
+    {
+        "picture": "10A",
+        "hover": "10B",
+        "name": "Bubble Shooter",
+        "category": "Party",
         "price": "$0.",
-        "cents": "99"
+        "cents": "25"
     },
     {
-        "picture": "05A",
-        "hover": "05B",
-        "name": "League Of Legends",
-        "category": "Multiplayer",
+        "picture": "11A",
+        "hover": "11B",
+        "name": "Scrabble Go",
+        "category": "Party",
         "price": "Free",
         "cents": ""
     },
     {
-        "picture": "06A",
-        "hover": "06B",
-        "name": "Valorant",
-        "category": "Multiplayer",
+        "picture": "12A",
+        "hover": "12B",
+        "name": "Wordle",
+        "category": "Party",
         "price": "$1.",
         "cents": "99"
     },
 ]
 let carrousel3 = [{
-        "picture": "01A",
-        "hover": "01B",
-        "name": "Among Us",
-        "category": "Multiplayer",
+        "picture": "13A",
+        "hover": "13B",
+        "name": "Assassin's Creed: Valhalla",
+        "category": "Role-playing",
         "price": "Free",
         "cents": ""
     },
     {
-        "picture": "02A",
-        "hover": "02B",
-        "name": "Fornite",
-        "category": "Multiplayer",
-        "price": "$1.",
-        "cents": "99"
+        "picture": "14A",
+        "hover": "14B",
+        "name": "Persona 5",
+        "category": "Role-playing",
+        "price": "$5.",
+        "cents": "25"
     },
     {
-        "picture": "03a",
-        "hover": "03B",
-        "name": "Minecraft",
-        "category": "Multiplayer",
+        "picture": "15A",
+        "hover": "15B",
+        "name": "Pokémon Arceus",
+        "category": "Role-playing",
         "price": "Free",
         "cents": ""
     },
     {
-        "picture": "04A",
-        "hover": "04B",
-        "name": "Fall Guys",
-        "category": "Multiplayer",
-        "price": "$0.",
-        "cents": "99"
+        "picture": "16A",
+        "hover": "16B",
+        "name": "Cyberpunk 2077",
+        "category": "Role-playing",
+        "price": "$89.",
+        "cents": "95"
     },
     {
-        "picture": "05A",
-        "hover": "05B",
-        "name": "League Of Legends",
-        "category": "Multiplayer",
+        "picture": "17A",
+        "hover": "17B",
+        "name": "Final Fantasy VII",
+        "category": "Role-playing",
         "price": "Free",
         "cents": ""
     },
     {
-        "picture": "06A",
-        "hover": "06B",
-        "name": "Valorant",
-        "category": "Multiplayer",
-        "price": "$1.",
+        "picture": "18A",
+        "hover": "18B",
+        "name": "South Park",
+        "category": "Role-playing",
+        "price": "$25.",
         "cents": "99"
     },
 ]
 let carrousel4 = [{
-        "picture": "01A",
-        "hover": "01B",
-        "name": "Among Us",
-        "category": "Multiplayer",
+        "picture": "19A",
+        "hover": "19B",
+        "name": "Microsoft Flight Simulator",
+        "category": "Simulation",
         "price": "Free",
         "cents": ""
     },
     {
-        "picture": "02A",
-        "hover": "02B",
-        "name": "Fornite",
-        "category": "Multiplayer",
-        "price": "$1.",
+        "picture": "20A",
+        "hover": "20B",
+        "name": "The Sims",
+        "category": "Simulation",
+        "price": "$22.",
+        "cents": "22"
+    },
+    {
+        "picture": "21A",
+        "hover": "21B",
+        "name": "Cooking Simulator",
+        "category": "Simulation",
+        "price": "Free",
+        "cents": ""
+    },
+    {
+        "picture": "22A",
+        "hover": "22B",
+        "name": "Arma III",
+        "category": "Simulation",
+        "price": "$11.",
         "cents": "99"
     },
     {
-        "picture": "03a",
-        "hover": "03B",
-        "name": "Minecraft",
-        "category": "Multiplayer",
+        "picture": "23A",
+        "hover": "23B",
+        "name": "Euro Truck Simulator 2",
+        "category": "Simulation",
         "price": "Free",
         "cents": ""
     },
     {
-        "picture": "04A",
-        "hover": "04B",
-        "name": "Fall Guys",
-        "category": "Multiplayer",
-        "price": "$0.",
-        "cents": "99"
-    },
-    {
-        "picture": "05A",
-        "hover": "05B",
-        "name": "League Of Legends",
-        "category": "Multiplayer",
-        "price": "Free",
-        "cents": ""
-    },
-    {
-        "picture": "06A",
-        "hover": "06B",
-        "name": "Valorant",
-        "category": "Multiplayer",
-        "price": "$1.",
+        "picture": "24A",
+        "hover": "24B",
+        "name": "Thief Simulator",
+        "category": "Simulation",
+        "price": "$13.",
         "cents": "99"
     },
 ]
 let carrousel5 = [{
-        "picture": "01A",
-        "hover": "01B",
-        "name": "Among Us",
-        "category": "Multiplayer",
+        "picture": "25A",
+        "hover": "25B",
+        "name": "Valheim",
+        "category": "Survival",
         "price": "Free",
         "cents": ""
     },
     {
-        "picture": "02A",
-        "hover": "02B",
-        "name": "Fornite",
-        "category": "Multiplayer",
-        "price": "$1.",
-        "cents": "99"
+        "picture": "26A",
+        "hover": "26B",
+        "name": "Frost Punk",
+        "category": "Survival",
+        "price": "$12.",
+        "cents": "33"
     },
     {
-        "picture": "03a",
-        "hover": "03B",
-        "name": "Minecraft",
-        "category": "Multiplayer",
+        "picture": "27A",
+        "hover": "27B",
+        "name": "Terraria",
+        "category": "Survival",
         "price": "Free",
         "cents": ""
     },
     {
-        "picture": "04A",
-        "hover": "04B",
-        "name": "Fall Guys",
-        "category": "Multiplayer",
-        "price": "$0.",
+        "picture": "28A",
+        "hover": "28B",
+        "name": "Astroneer",
+        "category": "Survival",
+        "price": "$18.",
         "cents": "99"
     },
     {
-        "picture": "05A",
-        "hover": "05B",
-        "name": "League Of Legends",
-        "category": "Multiplayer",
+        "picture": "29A",
+        "hover": "29B",
+        "name": "Among Trees",
+        "category": "Survival",
         "price": "Free",
         "cents": ""
     },
     {
-        "picture": "06A",
-        "hover": "06B",
-        "name": "Valorant",
-        "category": "Multiplayer",
-        "price": "$1.",
-        "cents": "99"
+        "picture": "30A",
+        "hover": "30B",
+        "name": "Resident Evil 4",
+        "category": "Horror",
+        "price": "$100.",
+        "cents": "00"
     },
 ]
 
@@ -257,13 +257,12 @@ function loadCarrousels() {
         for (let index = 0; index < carrousels[indice].length; index++) {
             let cardSmall = document.createElement("article");
             cardSmall.classList.add("card-small");
-
             let card = document.createElement("div");
             card.classList.add("close");
             let img = document.createElement("img");
-            img.src = `img/examples/${carrousel1[index].picture}.svg`;
+            img.src = `img/examples/${carrousels[indice][index].picture}.svg`;
             let gameName = document.createElement("p");
-            gameName.innerHTML = carrousel1[index].name;
+            gameName.innerHTML = carrousels[indice][index].name;
             gameName.classList.add("gameName")
             card.appendChild(img);
             card.appendChild(gameName);
@@ -273,7 +272,7 @@ function loadCarrousels() {
             hover.classList.add('open');
 
             let imgHover = document.createElement("img");
-            imgHover.src = `img/examples/${carrousel1[index].hover}.svg`;
+            imgHover.src = `img/examples/${carrousels[indice][index].hover}.svg`;
             imgHover.classList.add("imageSmall");
             hover.appendChild(imgHover);
 
@@ -289,12 +288,12 @@ function loadCarrousels() {
             content.appendChild(iconHeart);
 
             let title = document.createElement('p');
-            title.innerHTML = carrousel1[index].name;
+            title.innerHTML = carrousels[indice][index].name;
             title.classList.add("titleSmall");
             content.appendChild(title);
 
             let category = document.createElement('p');
-            category.innerHTML = carrousel1[index].category;
+            category.innerHTML = carrousels[indice][index].category;
             category.classList.add("bodySmall");
             content.appendChild(category);
 
@@ -303,14 +302,14 @@ function loadCarrousels() {
 
 
             let price = document.createElement('p');
-            price.innerHTML = carrousel1[index].price;
+            price.innerHTML = carrousels[indice][index].price;
             price.classList.add("bodyBold");
             let button = document.createElement('button');
             let buttonText = document.createElement('p');
             button.appendChild(buttonText);
-            if (carrousel1[index].price != "Free") {
+            if (carrousels[indice][index].price != "Free") {
                 let cents = document.createElement('span');
-                cents.innerHTML = carrousel1[index].cents;
+                cents.innerHTML = carrousels[indice][index].cents;
                 cents.classList.add("bodyBoldSmall");
                 price.appendChild(cents);
                 buttonText.innerHTML = "Buy";
