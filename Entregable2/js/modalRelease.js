951a92bd1267e851a0578a4a720bf6bd19cb4add
Sofia Martinez <sofiainesmartinez@gmail.com>
2022-10-10 15:23:12 -0300

    se agrega stop video cuando cierra modal en release

--- Entregable2/js/modalRelease.js
+++ Entregable2/js/modalRelease.js
@@ -10,4 +10,13 @@ btn.addEventListener('click', () => {
 
 close.addEventListener('click', () => {
     videoContainer.classList.remove('show');
+    var videos = document.querySelectorAll('iframe, video');
+    Array.prototype.forEach.call(videos, function (video) {
+        if (video.tagName.toLowerCase() === 'video') {
+            video.pause();
+        } else {
+            var src = video.src;
+            video.src = src;
+        }
+    });
 })
