e8004e6c56d5cfe3b0c09b3f38b1e470962352a2
Sofia Martinez <sofiainesmartinez@gmail.com>
2022-10-10 13:22:31 -0300

    loading home con css, se ajustan img release, se ajusta fat footer responsive

--- Entregable2/js/menuNav.js
+++ Entregable2/js/menuNav.js
@@ -1,34 +1,4 @@
 "use strict";
-
-let modal = document.querySelector("#loading");
-let bar = document.querySelector("#progressBar div");
-
-function load() {
-    modal.style.opacity = "0.95";
-    bar.parentElement.style.opacity = "1";
-    let width = 20;
-    let progressBar = setInterval(progress, 10);
-    function progress() {
-        if (width >= 100) {
-            clearInterval(progressBar);
-            completeLoad();
-        } else {
-            width++;
-            bar.style.width = width + '%';
-            bar.innerHTML = width * 1 + '%';
-        }
-    }
-}
-
-function completeLoad() {
-    modal.style.opacity = "0";
-    modal.style.zIndex = "-50"
-    bar.parentElement.style.opacity = "0";
-    // bar.parentElement.style.zIndex="-50"
-}
-
-load();
-
 document.addEventListener('DOMContentLoaded', function () {
     let btnMenu = document.querySelector("#btn-menu");
     let btnUser = document.querySelector('#btn-user');
