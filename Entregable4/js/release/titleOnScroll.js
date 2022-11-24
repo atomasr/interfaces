6ea63897a542c98008a2e7cd34992609562d95cd
Sofia Martinez <sofiainesmartinez@gmail.com>
2022-11-24 12:32:46 -0300

    se agrega efecto en cards

--- Entregable4/js/release/titleOnScroll.js
+++ Entregable4/js/release/titleOnScroll.js
@@ -1,19 +1,32 @@
 "use strict";
+document.addEventListener('DOMContentLoaded', function () {
+  var card = document.querySelectorAll(".card-character");
 
-function scrollTitle() {
+  function scrollTitle() {
     var aos = document.querySelectorAll(".aos");
-  
-    for (var i = 0; i < spriteTitle.length; i++) {
+
+
+    for (var i = 0; i < aos.length; i++) {
       var windowHeight = window.innerHeight;
       var elementTop = aos[i].getBoundingClientRect().top;
       var elementVisible = 150;
-  
+
       if (elementTop < windowHeight - elementVisible) {
         aos[i].classList.add("active");
+
+        for (var j = 0; j < card.length; j++) {
+          card[j].classList.add("animation-card");
+        }
+
       } else {
         aos[i].classList.remove("active");
+        for (var j = 0; j < card.length; j++) {
+          card[j].classList.remove("animation-card");
+        }
+
       }
     }
   }
-  
+
   window.addEventListener("scroll", scrollTitle);
+});
