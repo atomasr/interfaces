76307d3fe4ad23a67b9235a638cc2505f30fc41d
Tomás Rodríguez <augustotomasrodriguez@gmail.com>
2022-11-25 13:51:41 -0300

    Animación en columnas.

--- Entregable4/js/release/photoStory.js
+++ Entregable4/js/release/photoStory.js
@@ -8,20 +8,36 @@ let paths =
 
 let picture = document.getElementById("picStory");
 let txt = document.getElementById("text1");
+let i = 0;
 
 
 window.addEventListener('scroll',()=> {
   let top = Number(txt.offsetTop);
   let height = Number(txt.clientHeight);
 
-  if (window.scrollY > 0 && window.scrollY <= (top + 1.5*height)) {
-    picture.src = paths[0];
+  if (window.scrollY > 0 && window.scrollY <= (top + 1.5*height) && (i != 1)) {
+    i = 1;
+    picture.classList.remove("active");
+    setTimeout( function() {   
+      picture.src = paths[0];
+      picture.classList.add("active");
+    }, 50);
   }
-  if (window.scrollY > (top + 1.5*height) && window.scrollY <= (top + 2.5*height)) {
-    picture.src = paths[1];
+  if (window.scrollY > (top + 1.5*height) && window.scrollY <= (top + 2.5*height) && (i != 2)) {
+    i = 2;
+    picture.classList.remove("active");
+    setTimeout( function() {
+      picture.src = paths[1];
+      picture.classList.add("active");
+    }, 50);
   }
-  if (window.scrollY > (top + 2.5*height)) {
-    picture.src = paths[2];
+  if (window.scrollY > (top + 2.5*height) && (i != 3)) {
+    i = 3;
+    picture.classList.remove("active");
+    setTimeout( function() {
+      picture.src = paths[2];
+      picture.classList.add("active");
+    }, 50);
   }
 }
 );
